5537020e44ed986113bfb87057a91c05667f0191
Tuomas Borman <tvborm@utu.fi>
2025-08-23 07:52:31 +0300

    Add HMP_2019_ibdmdb files

---
+++ HMP_2019_ibdmdb/download_data.R
@@ -0,0 +1,36 @@
+
+dir <- "HMP_2019_ibdmdb"
+
+library(curatedMetagenomicData)
+library(ape)
+library(mia)
+
+# Load data from curatedMetagenomicData
+pattern <- "^2021-10-14\\.HMP_2019_ibdmdb\\.(pathway_abundance|relative_abundance)$"
+res <- curatedMetagenomicData(pattern = pattern, dryrun = FALSE, counts = TRUE, rownames = "short")
+
+# Save taxonomy data
+phylo <- res[[2]] |> rowTree()
+matches <- match(rowLinks(res[[2]])[[1L]], phylo$tip.label)
+phylo$tip.label[ matches ] <- rownames(res[[2]])
+phylo <- read.tree(file.path(dir, "phylogeny.tree"))
+rownames(res[[2]]) <- phylo$tip.label[ matches ]
+write.tree(phylo, file.path(dir, "phylogeny.tree"))
+tab <- res[[2]] |> rowData()
+write.csv(tab, file.path(dir, "taxonomy_table.csv"))
+tab <- res[[2]] |> colData()
+write.csv(tab, file.path(dir, "sample_metadata.csv"))
+tab <- res[[2]] |> assay()
+write.csv(tab, file.path(dir, "taxonomy_abundance.csv"))
+
+# Save pathway data
+tab <- res[[1]] |> rowData()
+tab[["taxonomy"]] <- rownames(res[[1]])
+tab <- mia:::.parse_taxonomy(
+    tab, column_name = "taxonomy", sep = "\\.")
+tab[["pathway"]] <- sub("\\|.*$", "", rownames(res[[1]]))
+rownames(tab) <- rownames(res[[1]])
+rowData(res[[1]]) <- tab
+res[[1]] <- agglomerateByVariable(res[[1]], by = 1L, group = "pathway")
+tab <- res[[1]] |> assay()
+write.csv(tab, file.path(dir, "pathway_abundance.csv"))
---
+++ HMP_2019_ibdmdb/import.R
@@ -0,0 +1,31 @@
+
+dir <- "HMP_2019_ibdmdb"
+
+library(mia)
+library(ape)
+
+# Read taxonomy data into TreeSE
+rd <- read.csv(file.path(dir, "taxonomy_table.csv"), row.names = 1L) |> DataFrame()
+cd <- read.csv(file.path(dir, "sample_metadata.csv"), row.names = 1L) |> DataFrame()
+assay <- read.csv(file.path(dir, "taxonomy_abundance.csv"), row.names = 1L) |> as.matrix()
+tree <- read.tree(file.path(dir, "phylogeny.tree"))
+tse1 <- TreeSummarizedExperiment(
+    assays = SimpleList(counts = assay),
+    rowData = rd,
+    colData = cd,
+    rowTree = tree
+)
+
+# Read pathway data into TreeSE
+assay <- read.csv(file.path(dir, "pathway_abundance.csv"), row.names = 1L) |> as.matrix()
+tse2 <- TreeSummarizedExperiment(
+    assays = SimpleList(relative_abundance = assay),
+    colData = cd
+)
+
+mae <- MultiAssayExperiment(
+    experiments = ExperimentList(taxonomy = tse1, pathway = tse2),
+    colData = cd
+)
+
+saveRDS(mae, file.path(dir, "mae.rds"))
