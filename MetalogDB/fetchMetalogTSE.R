b9d313d173c9518b008f84b61d3377485cff89ed
raivo-otus <hindstrom.rasmus@gmail.com>
2026-03-31 16:44:34 +0300

    working implementation

--- MetalogDB/fetchMetalogTSE.R
+++ MetalogDB/fetchMetalogTSE.R
@@ -6,10 +6,12 @@
 # Date: ---
 
 # Libraries
-library(mia)
+library(Matrix)
+library(TreeSummarizedExperiment)
 library(data.table)
 library(dplyr)
 library(httr)
+library(mia)
 
 # -------------
 # Helpers FUNs
@@ -141,6 +143,129 @@ library(httr)
 	))
 }
 
+# Function loads assay profile as sparse matrix
+.load_assay <- function(path, sep = "\t") {
+  # Read and subset to SGB only
+  dt <- fread(path, sep = sep)
+  dt <- dt[startsWith(clade_name, "t__SGB"), ]
+
+  # Ensure expected names and types
+  setnames(dt, c("sample_alias", "clade_name", "rel_abund"))
+  dt[, rel_abund := as.numeric(rel_abund)]
+  dt <- dt[!is.na(rel_abund) & rel_abund != 0]
+
+  # Aggregate duplicates (taxon, sample) -> sum(rel_abund)
+  setkey(dt, clade_name, sample_alias)
+  dt <- dt[, .(rel_abund = sum(rel_abund)), by = .(clade_name, sample_alias)]
+  # Map taxa and samples
+  taxa <- sort(unique(dt$clade_name))
+  samples <- sort(unique(dt$sample_alias))
+  i <- match(dt$clade_name, taxa)
+  j <- match(dt$sample_alias, samples)
+  x <- dt$rel_abund
+
+  # Build sparse matrix (rows = taxa, cols = samples)
+  X <- sparseMatrix(
+    i = i, j = j, x = x,
+    dims = c(length(taxa), length(samples)),
+    dimnames = list(taxa, samples)
+  )
+  assay <- list(assay = X, taxa = taxa, samples = samples)
+  return(assay)
+}
+
+# Loads the metadata and filteres to samples found in assay
+.load_metadata <- function(meta_df, samples, sep = "\t") {
+  dt <- fread(meta_df, sep = sep, na.strings = c("", "NA"))
+
+  # Pivot to wide
+  wide <- dcast(
+    dt,
+    sample_alias ~ metadata_item,
+    value.var = "value",
+    fill = NA_character_
+  )
+
+  meta_df <- as.data.frame(wide)
+  rownames(meta_df) <- meta_df$sample_alias
+
+  # Output Dataframe subset to samples
+  meta_df <- meta_df[samples, ]
+  return(meta_df)
+}
+
+# Maps full lineage names levels
+.construct_taxmap <- function(database, taxa) {
+  taxmap <- fread(database, sep = "\t", header = TRUE)
+  taxmap <- taxmap[startsWith(clade_name, "t__SGB")]
+
+  taxmap <- taxmap %>%
+    distinct(clade_name, .keep_all = TRUE) %>%
+    filter(clade_name %in% taxa)
+
+  idx <- match(taxa, taxmap$clade_name)
+  taxmap <- taxmap[idx, ]
+  
+  rownames(taxmap) <- taxmap$clade_name
+
+  taxmap <- taxmap %>%
+    filter(clade_name %in% taxa) %>%
+    tidyr::separate(
+      col = lineage,
+      into = c(
+        "Kingdom", "Phylum", "Class", "Order",
+        "Family", "Genus", "Species", "SGB"
+      ),
+      sep = "\\|",
+      fill = "right"
+    ) %>%
+    select(-NCBI_taxids, -clade_name) %>%
+    as.data.frame()
+
+  # Subset to taxa in present
+  taxmap <- taxmap[taxa, ]
+  return(taxmap)
+}
+
+# Function filters profile data down to provided samples
+.filter_datasets <- function(assay_list, samplelist) {
+  
+  # Resolve the samplelist into a vector of target IDs
+  ext <- tolower(tools::file_ext(samplelist))
+  
+  # Read file based on extension
+  if (ext %in% c("csv", "tsv")) {
+    sl_df <- data.table::fread(samplelist)
+  } else if (ext == "json") {
+    if (!requireNamespace("jsonlite", quietly = TRUE)) {
+      stop("The 'jsonlite' package is required to read JSON sample lists.")
+    }
+    sl_df <- as.data.frame(jsonlite::fromJSON(samplelist))
+  }
+  # Grab samples
+  target_samples <- sl_df[["sample_alias"]]
+  
+  # Intersect requested samples with available samples
+  available_samples <- assay_list[["samples"]]
+  keep_samples <- intersect(target_samples, available_samples)
+  
+  if (length(keep_samples) == 0) {
+    stop("Filtering Error: None of the provided samples were found in the dataset.")
+  }
+  
+  # Subset the sparse matrix (columns = samples)
+  assay_list$assay <- assay_list$assay[, keep_samples, drop = FALSE]
+  assay_list$samples <- keep_samples
+  
+  # Drop taxa that now have 0 abundance across all remaining samples
+  row_sums <- Matrix::rowSums(assay_list$assay)
+  keep_taxa <- names(row_sums[row_sums > 0])
+  assay_list$assay <- assay_list$assay[keep_taxa, , drop = FALSE]
+  assay_list$taxa <- keep_taxa
+  
+  return(assay_list)
+}
+
 # -------------
 # Main function
 # -------------
@@ -162,18 +287,32 @@ fetchMetalogTSE <- function(
 
   # Construct download URLs, download and cache
   data_files <- .resolve_metalog_url(collection, metadata, use_cache)
+  mapping_db <- .download_if_missing(
+    "https://metalog.embl.de/static/download/profiles/metaphlan4_clades.tsv.gz",
+    use_cache = use_cache
+  )
 
   # Injest data
-  
+  assay_list <- .load_assay(data_files[["assay"]])
 
   # Filtering
+  if (!is.null(samplelist)) {
+    message("Filtering datasets to include only the requested samples...")
+    assay_list <- .filter_datasets(assay_list, samplelist)
+  }
+  md_dt <- .load_metadata(data_files[["md"]], assay_list[["samples"]])
 
-  # TSE construction
+  # Construct TSE with full lineage mappings
+  tax <- .construct_taxmap(mapping_db, assay_list[["taxa"]]) 
+  
+  tse <- TreeSummarizedExperiment(
+    assays  = SimpleList("relabundance" = assay_list[["assay"]]),
+    colData = DataFrame(md_dt),
+    rowData = DataFrame(tax)
+  )
 
   # License injection
-
-  # Explicit return of tse object
-
+  metadata(tse)$license <- "https://metalog.embl.de/ - Open Database License (ODbL) v1.0"
+  
+  return(tse)
 }
-
-# Export function to namespace?
